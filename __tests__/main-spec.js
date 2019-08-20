dee956b96daaa6f4a20d9448a1b31f076c1a6df8
mmy <mmy_xize@163.com>
2019-08-20 21:32:17 +0800

    第10题代码提交

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,9 @@
-const add = require('../main');
+// const createMultiTable = require('../main');
+const showMultiTable = require('../main');
+it ('should show multiplication table from 2 to 4 when call showMultiTable given 2 to 4', () => {
+    expect(showMultiTable(2, 4)).toBe('2*2=4\t\n2*3=6\t3*3=9\t\n2*4=8\t3*4=12\t4*4=16\t\n');
+}) ;
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
-});
+// it ('should show multiplication table from 0 to 1 when call showMultiTable given 0 to 1', () => {
+//     expect(showMultiTable(0, 1)).toBe('2*2=4\t\n2*3=6\t3*3=9\t\n2*4=8\t3*4=12\t4*4=16\t\n');
+// }) ;
--- main.js
+++ main.js
@@ -1,5 +1,40 @@
-function add (left, right) {
-    return left + right;
+function showMultiTable (start, end) {
+    if (compareInputSize(start, end) && (checkInputRange(start, end))) {
+        createMultiTable(start, end);
+    } else {
+        return null;
+    }
 }
 
-module.exports = add;
+function compareInputSize (start, end) {
+    if (start < end) {
+        return true;
+    } else {
+        return null;
+    }
+}
+
+function checkInputRange(start, end) {
+    if ((start < 1 || start > 1000) || (end < 1 || end > 1000)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function createMultiTable(start, end) {
+    var result = '';
+    for (let i = start; i <= end; i++) {
+        var lineResult = '';
+        for (let j = start; j <= i; j++) {          
+            lineResult += (j + '*' + i + '=' + j*i + '\t'); 
+        } 
+        result += lineResult + '\n';            
+    }
+    return result;
+}
+
+// console.log(showMultiTable(2, 4));
+console.log(createMultiTable(2, 4));
+module.exports = createMultiTable;
+// module.exports = showMultiTable;
