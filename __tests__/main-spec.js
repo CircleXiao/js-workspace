ca08f488e251e6fa2caf3271ec9e756b3b174abd
mmy <mmy_xize@163.com>
2019-08-21 22:19:10 +0800

    修改乘法表中main.js及main-spec.js

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,9 +1,21 @@
 // const createMultiTable = require('../main');
 const showMultiTable = require('../main');
 it ('should show multiplication table from 2 to 4 when call showMultiTable given 2 to 4', () => {
-    expect(showMultiTable(2, 4)).toBe('2*2=4\t\n2*3=6\t3*3=9\t\n2*4=8\t3*4=12\t4*4=16\t\n');
+    expect(showMultiTable(2, 4)).toBe('2*2=4\n2*3=6\t3*3=9\n2*4=8\t3*4=12\t4*4=16\n');
 }) ;
 
-// it ('should show multiplication table from 0 to 1 when call showMultiTable given 0 to 1', () => {
-//     expect(showMultiTable(0, 1)).toBe('2*2=4\t\n2*3=6\t3*3=9\t\n2*4=8\t3*4=12\t4*4=16\t\n');
-// }) ;
+it ('should show multiplication table from 2 to 5 when call showMultiTable given 2 to 5', () => {
+    expect(showMultiTable(2, 5)).toBe('2*2=4\n2*3=6\t3*3=9\n2*4=8\t3*4=12\t4*4=16\n2*5=10\t3*5=15\t4*5=20\t5*5=25\n');
+}) ;
+
+it ('should show multiplication table from 0 to 1 when call showMultiTable given 0 to 1', () => {
+    expect(showMultiTable(0, 1)).toBe('两个参数均为1-1000之间的正整数！');
+}) ;
+
+it ('should show multiplication table from 3 to 2 when call showMultiTable given 3 to 2', () => {
+    expect(showMultiTable(3, 2)).toBe('第一个参数需要小于第二个参数！');
+}) ;
+
+it ('should show multiplication table from 1.1 to 2.1 when call showMultiTable given 1.1 to 2.1', () => {
+    expect(showMultiTable(1.1, 1.1)).toBe('输入的参数必须均为整数！');
+}) ;
--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 function showMultiTable (start, end) {
-    if (compareInputSize(start, end) && (checkInputRange(start, end))) {
-        createMultiTable(start, end);
+    var resultMsg = isInputValid(start, end);
+    if ( resultMsg === 'Success!') {
+        return createMultiTable(start, end);
     } else {
-        return null;
+        return resultMsg;
     }
 }
 
-function compareInputSize (start, end) {
+function isInputValid(start, end) {
+    var checkMsg = '';
+    if (!isInputInteger(start) || !isInputInteger(end)) {
+        checkMsg = '输入的参数必须均为整数！';
+    }else if (!isStartSmaller(start, end)) {
+        checkMsg = '第一个参数需要小于第二个参数！';
+    }else if (!isInScope(start) || !isInScope(end)) {
+        checkMsg = '两个参数均为1-1000之间的正整数！';
+    } else {
+        checkMsg = 'Success!';
+    }
+    return checkMsg;
+}
+
+function isInputInteger(param) {
+    return Number.isInteger(param);
+}
+
+function isStartSmaller(start, end) {
     if (start < end) {
         return true;
     } else {
-        return null;
+        return false;
     }
 }
 
-function checkInputRange(start, end) {
-    if ((start < 1 || start > 1000) || (end < 1 || end > 1000)) {
-        return false;
-    } else {
+function isInScope(param) {
+    if (param >= 1 && param <= 1000) {
         return true;
+    } else {
+        return false;
     }
 }
 
@@ -27,14 +46,14 @@ function createMultiTable(start, end) {
     for (let i = start; i <= end; i++) {
         var lineResult = '';
         for (let j = start; j <= i; j++) {          
-            lineResult += (j + '*' + i + '=' + j*i + '\t'); 
+            lineResult += (j + '*' + i + '=' + j*i); 
+            if (i !== j) {
+                lineResult += '\t';
+            }
         } 
         result += lineResult + '\n';            
     }
     return result;
 }
 
-// console.log(showMultiTable(2, 4));
-console.log(createMultiTable(2, 4));
-module.exports = createMultiTable;
-// module.exports = showMultiTable;
+module.exports = showMultiTable;
