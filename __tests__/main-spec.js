9a5d2b5b18ebac50201ec28b68003f09e6c1c835
mmy <mmy_xize@163.com>
2019-08-22 15:04:16 +0800

    修改测试用例

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,12 +1,11 @@
-// const createMultiTable = require('../main');
 // const showMultiTable = require('../main');
 const showMultiTable = require('../mainVersion2');
 it ('should show multiplication table from 2 to 4 when call showMultiTable given 2 to 4', () => {
-    expect(showMultiTable(2, 4)).toBe('2*2=4\n2*3=6\t3*3=9\n2*4=8\t3*4=12\t4*4=16\n');
+    expect(showMultiTable(2, 4)).toBe('2*2=4\n2*3=6\t3*3=9\n2*4=8\t3*4=12\t4*4=16');
 }) ;
 
 it ('should show multiplication table from 2 to 5 when call showMultiTable given 2 to 5', () => {
-    expect(showMultiTable(2, 5)).toBe('2*2=4\n2*3=6\t3*3=9\n2*4=8\t3*4=12\t4*4=16\n2*5=10\t3*5=15\t4*5=20\t5*5=25\n');
+    expect(showMultiTable(2, 5)).toBe('2*2=4\n2*3=6\t3*3=9\n2*4=8\t3*4=12\t4*4=16\n2*5=10\t3*5=15\t4*5=20\t5*5=25');
 }) ;
 
 it ('should show multiplication table from 0 to 1 when call showMultiTable given 0 to 1', () => {
